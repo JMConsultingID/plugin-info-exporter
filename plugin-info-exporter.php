499c90e3712f8d2575e5728951be3f8490fc2b70
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-07 10:06:24 +0800

    Update plugin-info-exporter.php

--- plugin-info-exporter.php
+++ plugin-info-exporter.php
@@ -18,20 +18,16 @@ add_action('admin_menu', 'pie_add_admin_page');
 function pie_admin_page_callback() {
     echo '<div class="wrap">';
     echo '<h1>Plugin Info Exporter</h1>';
-    echo '<p>Click the button below to export plugin information to JSON File:</p>';
-    echo '<form method="post">';
-    echo '<input type="submit" name="export_to_json" class="button button-primary" value="Export to JSON" />';
-    echo '</form>';
+    echo '<p>Click the button below to export plugin information to JSON:</p>';
+    echo '<button id="exportToJson" class="button button-primary">Export to JSON</button>';
     echo '</div>';
-
-    if(isset($_POST['export_to_json'])) {
-        pie_export_to_json();
-    }
 }
 
-function pie_export_to_json() {
-    ob_start(); // Start buffering at the very beginning
 
+add_action('wp_ajax_export_plugins_to_json', 'pie_export_to_json_ajax_handler');
+add_action('wp_ajax_nopriv_export_plugins_to_json', 'pie_export_to_json_ajax_handler');
+
+function pie_export_to_json_ajax_handler() {
     require_once(ABSPATH . 'wp-admin/includes/plugin.php');
     $all_plugins = get_plugins();
     $active_plugins = get_option('active_plugins');
@@ -54,13 +50,36 @@ function pie_export_to_json() {
         $plugins_data[] = $plugin_info;
     }
 
-    $json_data = json_encode($plugins_data, JSON_PRETTY_PRINT);
-
-    header('Content-Type: application/json');
-    header('Content-Disposition: attachment;filename="plugin-info-' . time() . '.json"');
-    header('Cache-Control: max-age=0');
-
-    echo $json_data;
-    ob_end_clean(); // Hentikan buffering dan bersihkan output
-    exit;
+    echo json_encode($plugins_data);
+    wp_die(); // This is important to terminate immediately and return a proper response
 }
+
+
+function pie_enqueue_admin_scripts() {
+    echo "
+    <script type='text/javascript'>
+    jQuery(document).ready(function($) {
+        $('#exportToJson').click(function() {
+            $.ajax({
+                url: ajaxurl,
+                data: {
+                    'action': 'export_plugins_to_json'
+                },
+                success: function(data) {
+                    // Create a blob and trigger a download
+                    var blob = new Blob([data], {type: 'application/json'}),
+                        a = document.createElement('a');
+                    a.href = URL.createObjectURL(blob);
+                    a.download = 'plugin-info-' + new Date().getTime() + '.json';
+                    a.click();
+                },
+                error: function(errorThrown){
+                    console.log(errorThrown);
+                }
+            });
+        });
+    });
+    </script>
+    ";
+}
+add_action('admin_footer', 'pie_enqueue_admin_scripts');
